f0390b48c5eb8f739d71bc80a0e80bad7c3b76db
Sara Hopper <sara.hopper@gmail.com>
2017-10-09 13:31:48 -0400

    update devise to qork with jquery"

--- app/assets/javascripts/gameindex.js
+++ app/assets/javascripts/gameindex.js
@@ -14,7 +14,13 @@
 
 
 
-
+$("form#sign_in_user").bind "ajax:success", (e, data, status, xhr) ->
+    if data.success
+      $('#sign_in').modal('hide')
+      $('#sign_in_button').hide()
+      $('#submit_comment').slideToggle(1000, "easeOutBack" )
+    else
+      alert('failure!')
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { sessions: 'sessions', :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'application#index'
   get '/home', to: 'users#home', as: 'home'
 
