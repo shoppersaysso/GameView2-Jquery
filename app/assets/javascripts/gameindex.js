0b42250c3f3ffa0e2fb7129e87c8983184dab929
Sara Hopper <sara.hopper@gmail.com>
2017-10-09 13:37:27 -0400

    index switches from sign-up to sign-in

--- app/assets/javascripts/gameindex.js
+++ app/assets/javascripts/gameindex.js
@@ -1,51 +1,28 @@
+//
+//
+// $("form#sign_in_user").bind "ajax:success", (e, data, status, xhr) ->
+//     if data.success
+//       $('#sign_in').modal('hide')
+//       $('#sign_in_button').hide()
+//       $('#submit_comment').slideToggle(1000, "easeOutBack" )
+//     else
+//       alert('failure!')
+//
+//
+//
+//
+//
+//
+//
+$(document).ready(function(){
+    $("#showSignUp").click(function(){
+        $("#login").addClass("hidden");
+        $("#signup").removeClass("hidden");
 
+    });
+    $("#showLogIn").click(function(){
+        $("#login").removeClass("hidden");
+        $("#signup").addClass("hidden");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$("form#sign_in_user").bind "ajax:success", (e, data, status, xhr) ->
-    if data.success
-      $('#sign_in').modal('hide')
-      $('#sign_in_button').hide()
-      $('#submit_comment').slideToggle(1000, "easeOutBack" )
-    else
-      alert('failure!')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function() {
-
-})
+    });
+});
